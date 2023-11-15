3b150879d533030ada134b890ac241278eec0efe
Altair1618 <farhannabilsuryono3@gmail.com>
2023-11-15 23:03:55 +0700

    feat: implement register
    Co-Authored-By: Mitahul03 <113118888+Mitahul03@users.noreply.github.com>

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,7 +1,128 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { toast } from "react-toastify";
+
+import { Brand } from "@/components/brand/brand";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Link, useNavigate } from "react-router-dom";
+
+const formSchema = z.object({
+  credentials: z.string(),
+  password: z.string(),
+});
+
 export function Register() {
+  const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      credentials: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const url = `${import.meta.env.VITE_REST_SERVICE}/register`;
+
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === "success") {
+          toast.success(res.message);
+          navigate("/login");
+        } else {
+          toast.error(res.message);
+        }
+      });
+  }
+
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="flex h-full w-full grow flex-col items-center justify-center gap-[45px] px-[35px]">
+      <Brand size="large" clickable={false} />
+
+      <div className="flex w-full max-w-[450px] flex-col justify-center gap-[35px] rounded-[20px] bg-white px-5 py-8">
+        <p className="text-center text-2xl font-bold">SIGN UP</p>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col justify-center space-y-8"
+          >
+            <div className="flex flex-col gap-2.5">
+              <FormField
+                control={form.control}
+                name="credentials"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username/Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        required
+                        placeholder="Masukkan Username atau Email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        required
+                        type="password"
+                        placeholder="Masukkan Password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription></FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <Button
+              type="submit"
+              className="rounded-full bg-blue-700 text-sm font-bold text-white"
+            >
+              SIGN UP
+            </Button>
+          </form>
+        </Form>
+
+        <p className="text-center text-sm">
+          Sudah memiliki akun?{" "}
+          <Link to="/login" className="text-blue-500 underline">
+            Sign In
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
